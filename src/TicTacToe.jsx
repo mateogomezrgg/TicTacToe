3e76cb423ea8b5ad59ae9ebb906e98dcdf123a7e
Mateo Gomez <mateojjoaquin3@gmail.com>
2023-06-05 23:19:56 -0300

    The game it's working. But still need add some styles and the functionality of the players score.

--- src/TicTacToe.jsx
+++ src/TicTacToe.jsx
@@ -1,59 +1,59 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 
 export const TicTacToe = () => {
-  const [turn, setTurn] = useState("X");
+  const [turn, setTurn] = useState(true);
 
-  const [ahoraVieneX, setAhoraVieneX] = useState(true);
+  const [board, setBoard] = useState(Array(9).fill(null));
 
-  const [OcuppedCells, setOcuppedCells] = useState(false);
+  const winConditions = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
 
-  // const [winner, setWinner] = useState([]);
-
-  const handleClick = () => {
-    setTurn((turn) => (turn === "X" ? "O" : "X"));
-    // if (turn === "X" && OcuppedCells === false) {
-    //   setOcuppedCells(true);
-    //   setAhoraVieneX(false);
-    //   turn === "O";
-    // }
-    console.log(OcuppedCells);
-  };
-
-  const cellClicked = () => {
-    document.getElementById("r1c1").onclick = function () {
-      if (turn === "X") {
-        document.getElementById("r1c1").innerHTML = "X";
-      } else if (turn == "O") {
-        document.getElementById("r1c1").innerHTML = "O";
+  const onHandleClick = (index) => {
+    const updatedBoard = board.map((value, idx) => {
+      if (idx === index) {
+        return turn ? "x" : "o";
+      } else {
+        return value;
       }
-    };
+    });
+
+    setBoard(updatedBoard);
+    handleWin(updatedBoard);
+    setTurn(!turn);
   };
 
-  useEffect(() => {
-    if (turn === "X") {
-      document.getElementsByClassName("cell")[5].innerHTML = "X";
-    } else {
-      document.getElementsByClassName("cell")[5].innerHTML = "O";
-    }
-    console.log("useEffect turn: " + turn);
-  }, [turn]);
+  const handleWin = (board) => {
+    winConditions.forEach((value) => {
+      const [x, y, z] = value || [];
 
-  console.log("turno de " + turn);
+      if (board[x] && board[x] == board[y] && board[x] == board[z]) {
+        alert(`The winner are the : ${board[x]} !!! Congrats`);
+        return board[x];
+      }
+    });
+  };
+
   return (
-    <div className="board">
-      <div
-        className="cell"
-        id="r1c1"
-        onClick={(handleClick, cellClicked)}
-      ></div>
-      <div className="cell" id="r1c2" onClick={handleClick} value="X"></div>
-      <div className="cell" id="r1c3" onClick={handleClick}></div>
-      <div className="cell" id="r2c1" onClick={handleClick}></div>
-      <div className="cell" id="r2c2" onClick={handleClick}></div>
-      <div className="cell" id="r2c3" onClick={handleClick}></div>
-      <div className="cell" id="r3c1" onClick={handleClick}></div>
-      <div className="cell" id="r3c2" onClick={handleClick}></div>
-      <div className="cell" id="r3c3" onClick={handleClick}></div>
+    <div className="board" style={{ height: 200 }}>
+      {board.map((value, index) => {
+        return (
+          <div
+            className="cell"
+            key={index}
+            onClick={() => !value && onHandleClick(index)}
+          >
+            {value}
+          </div>
+        );
+      })}
     </div>
   );
 };
