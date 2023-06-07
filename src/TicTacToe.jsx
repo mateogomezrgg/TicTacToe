ce3eaa8d0c517122ec88c488896455961b9b3286
Mateo Gomez <mateojjoaquin3@gmail.com>
2023-06-07 18:47:08 -0300

    still need add some styles... but it's working fine.

--- src/TicTacToe.jsx
+++ src/TicTacToe.jsx
@@ -1,9 +1,31 @@
 import { useState } from "react";
+import classNames from "classnames";
+
+const Player = {
+  X: "X",
+  O: "O",
+};
+
+const Counter = {
+  X: 0,
+  O: 0,
+};
+
+const INITIAL_STATUS = Array(9).fill(null);
 
 export const TicTacToe = () => {
   const [turn, setTurn] = useState(true);
 
-  const [board, setBoard] = useState(Array(9).fill(null));
+  const [board, setBoard] = useState(INITIAL_STATUS);
+
+  const Estado = {
+    Playing: true,
+    Draw: false,
+    XWON: false,
+    OWON: false,
+  };
+
+  const [status, setStatus] = useState(Estado.Playing);
 
   const winConditions = [
     [0, 1, 2],
@@ -16,44 +38,90 @@ export const TicTacToe = () => {
     [2, 4, 6],
   ];
 
+  let cellClass = classNames({
+    board: true,
+    [`Counter--${turn}`]: board !== null,
+  });
+
   const onHandleClick = (index) => {
+    if (status !== true) return;
+
     const updatedBoard = board.map((value, idx) => {
       if (idx === index) {
-        return turn ? "x" : "o";
+        return turn ? Player.X : Player.O;
       } else {
         return value;
       }
     });
 
     setBoard(updatedBoard);
-    handleWin(updatedBoard);
     setTurn(!turn);
+    handleWin(updatedBoard);
   };
 
   const handleWin = (board) => {
+    let winner = Player || undefined;
+
     winConditions.forEach((value) => {
       const [x, y, z] = value || [];
 
       if (board[x] && board[x] == board[y] && board[x] == board[z]) {
-        alert(`The winner are the : ${board[x]} !!! Congrats`);
-        return board[x];
+        winner = board[x];
+        return winner;
       }
     });
+
+    if (winner === Player.X) {
+      setStatus((Estado.XWON = true));
+      setStatus(false);
+      Counter.X++;
+      alert("El ganador es X!");
+    } else if (winner === Player.O) {
+      setStatus((Estado.OWON = true));
+      setStatus(false);
+      Counter.O++;
+      alert("El ganador es O!");
+    } else if (board.every((cell) => [Player.X, Player.O].includes(cell))) {
+      setStatus((Estado.Draw = true));
+      setStatus(false);
+      alert("Empate!!!");
+    }
+  };
+
+  const onHandleReset = () => {
+    setBoard(INITIAL_STATUS);
+    setStatus(true);
+    setTurn(true); // ésta línea vuelve a comenzar las partidas con X jugando
   };
 
   return (
-    <div className="board" style={{ height: 200 }}>
-      {board.map((value, index) => {
-        return (
-          <div
-            className="cell"
-            key={index}
-            onClick={() => !value && onHandleClick(index)}
-          >
-            {value}
-          </div>
-        );
-      })}
-    </div>
+    <main>
+      <section className="turnOf">
+        <p>Turno de: {turn ? Player.X : Player.O}</p>
+        <div className="counterContainer">
+          <p className="counterX">X Ganó: {Counter.X}</p>
+          <p className="counterO">O Ganó: {Counter.O}</p>
+        </div>
+      </section>
+      <div className={cellClass}>
+        {board.map((value, index) => {
+          return (
+            <div
+              className="cell"
+              key={index}
+              onClick={() => !value && onHandleClick(index)}
+            >
+              {value}
+            </div>
+          );
+        })}
+        <section className="results">
+          <article id="alert" role="alert"></article>
+          <button id="btnReset" onClick={onHandleReset}>
+            Reset
+          </button>
+        </section>
+      </div>
+    </main>
   );
 };
